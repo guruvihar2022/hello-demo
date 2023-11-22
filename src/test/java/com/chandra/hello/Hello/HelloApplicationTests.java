7035e69ab0c194daf2363a2430e5d39dc833882b
unknown <guruvihar2022@gmail.com>
2023-11-22 09:57:44 +0530

    first commit

---
+++ src/test/java/com/chandra/hello/Hello/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandra.hello.Hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
