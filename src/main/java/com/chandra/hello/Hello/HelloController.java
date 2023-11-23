583d32e06c75bd4c52dd45c6008e79b869e63ef7
guruvihar2022 <140685968+guruvihar2022@users.noreply.github.com>
2023-11-23 09:58:43 +0530

    Update HelloController.java

--- src/main/java/com/chandra/hello/Hello/HelloController.java
+++ src/main/java/com/chandra/hello/Hello/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
 	public String helloWorld() {
-		return "hello";
+		return "hellooo";
 	}
     
 }
